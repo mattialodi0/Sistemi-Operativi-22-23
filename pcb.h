6beaac95c5109f6c3a59dfea99d82d16ce4e5e6a
Gianluca di Giacomo <82754142+Giangi1902@users.noreply.github.com>
2023-02-04 17:05:53 +0100

    Update pcb.h

--- pcb.h
+++ pcb.h
@@ -1,103 +1,48 @@
+#ifndef PCB_H
+#define PCB_H
+
 #include "pandos_types.h"
+#include "pandos_const.h"
 #include "list.h"
 
-int pcbFree_table[MAXPROC];
 
 //Funzione 1
-void initPcbs(){
-    for(int i=0; i<MAXPROC; i++){
-        //list_add(pcbFree_table[i], pcbFree_h);
-    }
-}
-
+void initPcbs(){}
 
 //Funzione 2
-void freePcb(pcb_t *p){
-    //list_add(p, pcbFree_h);
-}
-
+void freePcb(pcb_t *p){}
 
 //Funzione 3
-pcb_t *allocPcb(){
-/*  new pcblist per salvare l'elemento rimosso per poi restituirlo
-    if(list_empty(pcbFree_h) == 1){
-        return NULL;
-    }
-    else{
-        salvare l'elemento nella new pcblist
-        list_del(pcbFree_h);
-        ???
-        ritornare pcblist
-    } 
-*/
-}
-
+pcb_t *allocPcb(){}
 
 //Funzione 4
-
-void mkEmptyProcQ(struct list_head *head){
-    
-}
-
+void mkEmptyProcQ(struct list_head *head){}
 
 //Funzione 5
-
-int emptyProcQ(struct list_head *head){
-    return list_empty(head);
-}
-
+int emptyProcQ(struct list_head *head){}
 
 //Funzione 6
-
-void insertProcQ(struct list_head *head, pcb_t *p){
-    list_add_tail(p, head);
-}
-
+void insertProcQ(struct list_head *head, pcb_t *p){}
 
 //Funzione 7
-
-pcb_t *headProcQ(struct list_head *head){
-    
-}
+pcb_t *headProcQ(struct list_head *head){}
 
 //Funzione 8
-pcb_t *removeProcQ(struct list_head *head){
-
-}
-
+pcb_t *removeProcQ(struct list_head *head){}
 
 //Funzione 9
-pcb_t *outProcQ(struct list_head *head, pcb_t *p){
-
-}
-
+pcb_t *outProcQ(struct list_head *head, pcb_t *p){}
 
 //Funzione 10
-int emptyChild(pcb_t *p){
-/*    if(p->p_child == NULL){
-        return true;
-    }
-    else{
-        return false;
-    }
-*/
-}
-
+int emptyChild(pcb_t *p){}
 
 //Funzione 11
-void insertChild(pcb_t *prnt, pcb_t *p){
-
-}
-
+void insertChild(pcb_t *prnt, pcb_t *p){}
 
 //Funzione 12
-pcb_t *removeChild(pcb_t *p){
-
-}
-
+pcb_t *removeChild(pcb_t *p){}
 
 //Funzione 13
-pcb_t *outChild(pcb_t *p){
+pcb_t *outChild(pcb_t *p){}
 
-}
-
+#endif
