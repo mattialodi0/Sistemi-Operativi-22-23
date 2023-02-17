ef364e635615ef208a8cd06ace0da30c6d096841
Gianluca di Giacomo <82754142+Giangi1902@users.noreply.github.com>
2023-02-17 16:01:12 +0100

    Update pcb.c

--- pcb.c
+++ pcb.c
@@ -7,9 +7,11 @@ HIDDEN pcb_t pcbFree_table[MAXPROC];    /*array di pcb con dimensione massima MA
 /*Inizializza la lista pcbFree_h in modo da contenere gli elementi della pcbFree_table*/
 void initPcbs(){
     INIT_LIST_HEAD(&pcbFree_h);
-    for(int i=0; i<MAXPROC ; i++){      /*Con un ciclo inserisco di iterazione in iterazione gli elementi della pcbFree_table in pcbFree_h*/
+    int i=0;
+    while(i<MAXPROC){        /*Con un ciclo inserisco di iterazione in iterazione gli elementi della pcbFree_table in pcbFree_h*/
         INIT_LIST_HEAD(&pcbFree_table[i].p_list);
         list_add(&pcbFree_table[i].p_list, &pcbFree_h);
+        i++;
     }
 }
 
@@ -27,16 +29,15 @@ pcb_t *allocPcb(){
     } 
     else{
         pcb_t *tmp = container_of(pcbFree_h.next, pcb_t, p_list);   /*In tmp salvo l'elemento che andrà rimosso*/
-        list_del(pcbFree_h.next);       /*Rimuovo l'elemento dalla pcbFree_h*/
-        tmp->p_parent = NULL;           /*Inizializzo tutti i campi dell'elemento rimosso a NULL / 0*/
+        list_del(pcbFree_h.next);                                   /*Rimuovo l'elemento dalla pcbFree_h*/
+        tmp->p_parent = NULL;                                       /*Inizializzo tutti i campi dell'elemento rimosso a NULL/0 */
         INIT_LIST_HEAD(&tmp->p_child);
         INIT_LIST_HEAD(&tmp->p_sib);
-        tmp->p_time = 0;
         tmp->p_semAdd = NULL;
         for(int i=0; i < NS_TYPE_MAX; i++){
             tmp->namespaces[i] = NULL;
         }
-        return tmp;     /*Restituisco l'elemento rimosso*/
+        return tmp;                                                 /*Restituisco l'elemento rimosso*/
     }
 }
 
@@ -61,7 +62,7 @@ void insertProcQ(struct list_head *head, pcb_t *p){
 
 /*Restituisce l'elemento di testa della coda dei processi puntata da head senza rimuoverlo*/
 pcb_t *headProcQ(struct list_head *head){
-    if(list_empty(head) == 1){      /*Se head è vuota restituisco NULL*/
+    if(list_empty(head) == 1){                                   /*Se head è vuota restituisco NULL*/
         return NULL;
     }
     else{
@@ -72,13 +73,13 @@ pcb_t *headProcQ(struct list_head *head){
 
 /*Rimuove il primo elemento della coda dei processi puntata da head*/
 pcb_t *removeProcQ(struct list_head *head){
-    if(list_empty(head) == 1){      /*Se head è vuota restituisco NULL*/
+    if(list_empty(head) == 1){                                      /*Se head è vuota restituisco NULL*/
         return NULL;
     }
     else{
         pcb_t *tmp = container_of(head->next, pcb_t, p_list);       /*Salvo in tmp il primo elemento della coda puntata da head*/
-        list_del(head->next);       /*Rimuovo il primo elemento della coda puntata da head*/
-        return tmp;     /*Restituisco l'elemento rimosso salvato precedentemente*/
+        list_del(head->next);                                       /*Rimuovo il primo elemento della coda puntata da head*/
+        return tmp;                                                 /*Restituisco l'elemento rimosso salvato precedentemente*/
     } 
 }
 
@@ -89,43 +90,43 @@ pcb_t *outProcQ(struct list_head *head, pcb_t *p){
         return NULL;
     }
     else{
-        struct list_head *tmp = head->next;     /*Creo una lista temporanea da scorrere*/
-        while(tmp != head){    /*Con il ciclo while scorro la lista temporanea creata precedentemente*/
+        struct list_head *tmp = head->next;                 /*Creo una lista temporanea da scorrere*/
+        while(tmp != head){                                 /*Con il ciclo while scorro la lista temporanea creata precedentemente*/
             pcb_t *t = container_of(tmp, pcb_t, p_list);    /*Salvo in t l'elemento corrente della lista*/
-            if(t == p){     /*Verifico se l'elemento corrente è p, ovvero l'elemento da rimuovere, se è così lo rimuovo*/
+            if(t == p){                                     /*Verifico se l'elemento corrente è p, ovvero l'elemento da rimuovere, se è così lo rimuovo*/
                 list_del(tmp);
                 return t;
             }
             tmp = tmp->next;  
         }
-        return NULL;    /*Se p non è stato trovato precedentemente (e quindi non è presente nella coda) restituisco NULL*/
+        return NULL;                                        /*Se p non è stato trovato precedentemente (e quindi non è presente nella coda) restituisco NULL*/
     }
 }
 
 
 /*Se il pcb puntato da p non ha figli restituisce TRUE, altrimenti FALSE*/
 int emptyChild(pcb_t *p){
-    return (list_empty(&p->p_child));       /*Verifico tramite list_empty se il pcb puntato da p ha figli o meno*/
+    return (list_empty(&p->p_child));               /*Verifico tramite list_empty se il pcb puntato da p ha figli o meno*/
 }
 
 
 /*Inserisce il pcb puntato da p come figlio del pcb puntato da prnt*/
 void insertChild(pcb_t *prnt, pcb_t *p){
-    p->p_parent = prnt; 
+    p->p_parent = prnt;                             /*inizializzo prnt come parent per l'aggiunta successiva dei figli*/
     list_add_tail(&p->p_sib, &prnt->p_child);       /*aggiungo il pcb puntato da p come figlio del pcb puntato da prnt*/
 }
 
 
 /*Rimuove il primo figlio del pcb puntato da p*/
 pcb_t *removeChild(pcb_t *p){
-    if (list_empty(&p->p_child) == 1){      /*Se p non ha figli restituisco NULL*/
+    if (list_empty(&p->p_child) == 1){          /*Se p non ha figli restituisco NULL*/
         return NULL;
     }
     else{
         pcb_t *tmp = container_of(list_next(&p->p_child), pcb_t, p_sib);
-        list_del(&tmp->p_sib);      /*Rimuovo il primo figlio del pcb puntato da p*/
+        list_del(&tmp->p_sib);                  /*Rimuovo il primo figlio del pcb puntato da p*/
         tmp->p_parent = NULL;
-        return tmp;     /*Restituisco il figlio rimosso*/
+        return tmp;                             /*Restituisco il figlio rimosso*/
     }
 }
 
@@ -137,9 +138,8 @@ pcb_t *outChild(pcb_t *p){
         return  NULL;
     }
     else{
-        list_del(&p->p_sib);        /*RImuovo il pcb puntato da p*/
+        list_del(&p->p_sib);        /*Rimuovo il pcb puntato da p*/
         p->p_parent = NULL;
-        return p;       /*Restituisco l'elemento rimosso*/
+        return p;                   /*Restituisco l'elemento rimosso*/
     }
 }
-
