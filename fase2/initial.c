969006dc21dd258c67dfcae39cb4c78b0e5482a4
mattyk0207 <mattia.lodi2002@gmail.com>
2023-04-11 12:31:52 +0200

    agguinto il pass up vector

--- fase2/initial.c
+++ fase2/initial.c
@@ -3,9 +3,10 @@
 #include <pandos_types.h>
 
 #include <umps3/umps/libumps.h>
-#include "pcb.h"
-#include "ash.h"
-#include "ns.h"
+#include <pcb.h>
+#include <ash.h>
+#include <ns.h>
+#include "scheduler.h"
 
 #define DISPNUM 49
 
@@ -21,7 +22,7 @@ int main(void) {
     mkEmptyProcQ(ready_queue);
 
     //puntatore al proc attivo
-    pcb_t *active_process = NULL;
+    pcb_t *active_process = NULL;   
 
     //array di semafori, uno per dispositivo
     int sem_disp[DISPNUM];
@@ -33,7 +34,12 @@ int main(void) {
     initSemd();
     initNS();
 
-    //pass up vector
+    //pass up vector, for processor 0 at 0x0FFF.F900
+    0x0FFF.F900->tlb_refll_handler = (memaddr) uTLB_RefillHandler;    // set the Nucleus TLB-Refill event handler address
+    (0x0FFF.F900 + 0x04)* = 0x2000.1000;    // set the Stack Pointer for the Nucleus TLB-Refill event handler to the top of the Nucleus stack page: 0x2000.1000
+    (0x0FFF.F900 + 0x08)->exception_handler = (memaddr) fooBar;   // set the Nucleus exception handler address to the address of your Level 3 Nucleus function that is to be the entry point for exception handling 
+    (0x0FFF.F900 + 0x0c)* = 0x2000.1000;    // set the Stack pointer for the Nucleus exception handler to the top of the Nucleus stack page: 0x2000.1000
+
 
     //iniz. interval timer 100ms
     //registro:  0x1000.0020
@@ -42,4 +48,7 @@ int main(void) {
     //creazione di un processo
 
     //chiamata allo scheduler
+    scheduler();
 }
+
+//non so se sia corretto il pass upo vector, è un po strano il modo in cui sono usati gli ind. come puntatori
