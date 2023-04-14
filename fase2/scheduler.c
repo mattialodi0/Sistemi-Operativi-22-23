b842447749f8d82f2d52f82070760fd373f41d9a
mattyk0207 <mattia.lodi2002@gmail.com>
2023-04-14 16:48:47 +0200

    iniziato lo scheduler

--- fase2/scheduler.c
+++ fase2/scheduler.c
@@ -1,4 +1,4 @@
-#include ``/usr/include/umps3/umps/libumps.h''
+#include "scheduler.h"
 
 //scelta pcb 
 //rimozione pcb dalla testa e tornare il puntatore al pcb nel campo dei processi correnti
@@ -8,3 +8,30 @@
 //se il process count è 0 richiama l'HALT BIOS
 //se il process count è > 0 e il Soft-Block Count > 0 va in Wait State 
 //controllo se process count > 0 e Soft-Block Count == 0 => deadlock. ???. Richiamare PANIC BIOS.
+
+extern struct list_head *ready_queue; 
+extern pcb_t *active_process = NULL;   
+extern int process_count;
+extern int soft_blocked_count;
+
+void scheduler() {
+    active_process = removeProcQ(ready_queue);
+    
+    //load PLT
+
+    //load state
+
+    if(process_count == 0) {
+        //halt 
+    }
+    else if(process_count > 0) {
+        if(soft_blocked_count > 0) {
+            //abilita gli interrupt
+            //WAIT
+        }
+        else if(soft_blocked_count == 0) {
+            //deadlock detection
+            //WAIT
+        }
+    }
+}
--- fase2/scheduler.h
+++ fase2/scheduler.h
@@ -1,2 +1,9 @@
+#include <pandos_const.h>
+#include <pandos_types.h>
+
+#include <umps3/umps/libumps.h>
+#include <pcb.h>
+#include <ash.h>
+#include <ns.h>
 
 void scheduler();
