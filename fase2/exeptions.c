3aea8e11f6e5005eb5c299a76618726d879c1e86
mattyk0207 <mattia.lodi2002@gmail.com>
2023-04-16 17:08:27 +0200

    .

--- fase2/exeptions.c
+++ fase2/exeptions.c
@@ -1,42 +1,42 @@
 #include </usr/include/umps3/umps/libumps.h>
 #include "include/types.h"
 
-int Create_Process(CREATEPROCESS, state_t *statep, support_t *supportp, nsd_t *ns){
+int Create_Process(state_t *statep, support_t *supportp, nsd_t *ns){
     
 }
 
-void Terminate_Process(TERMPROCESS, int pid, 0, 0){
+void Terminate_Process(int pid){
 
 }
 
-void Passeren(PASSEREN, int *semaddr, 0, 0){
+void Passeren(int *semaddr){
 
 }
 
-void Verhogen(VERHOGEN, int *semaddr, 0, 0){
+void Verhogen(int *semaddr){
 
 }
 
-int DO_IO(DOIO, int *cmdAddr, int *cmdValues, 0){
+int DO_IO(int *cmdAddr, int *cmdValues){
 
 }
 
-int Get_CPU_Time(GETTIME, 0, 0, 0){
+int Get_CPU_Time(){
 
 }
 
-int Wait_For_Clock(CLOCKWAIT, 0, 0, 0){
+int Wait_For_Clock(){
 
 }
 
-support_t* Get_Support_Data(GETSUPPORTPTR, 0, 0, 0){
+support_t* Get_Support_Data(){
 
 }
 
-int Get_Process_Id(GETPROCESSID, int parent, 0, 0){
+int Get_Process_Id(int parent){
 
 }
 
-int Get_Children(GET_CHILDREN, int *children, int size, 0){
+int Get_Children(int *children, int size){
 
 }
