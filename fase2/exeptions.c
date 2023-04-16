970a8be39c59c0c4907dcbab0f11c46de7788902
Gianluca di Giacomo <82754142+Giangi1902@users.noreply.github.com>
2023-04-16 16:55:37 +0200

    Update exeptions.c

--- fase2/exeptions.c
+++ fase2/exeptions.c
@@ -1,1 +1,42 @@
-#include ``/usr/include/umps3/umps/libumps.h''
+#include </usr/include/umps3/umps/libumps.h>
+#include "include/types.h"
+
+int Create_Process(CREATEPROCESS, state_t *statep, support_t *supportp, nsd_t *ns){
+    
+}
+
+void Terminate_Process(TERMPROCESS, int pid, 0, 0){
+
+}
+
+void Passeren(PASSEREN, int *semaddr, 0, 0){
+
+}
+
+void Verhogen(VERHOGEN, int *semaddr, 0, 0){
+
+}
+
+int DO_IO(DOIO, int *cmdAddr, int *cmdValues, 0){
+
+}
+
+int Get_CPU_Time(GETTIME, 0, 0, 0){
+
+}
+
+int Wait_For_Clock(CLOCKWAIT, 0, 0, 0){
+
+}
+
+support_t* Get_Support_Data(GETSUPPORTPTR, 0, 0, 0){
+
+}
+
+int Get_Process_Id(GETPROCESSID, int parent, 0, 0){
+
+}
+
+int Get_Children(GET_CHILDREN, int *children, int size, 0){
+
+}
